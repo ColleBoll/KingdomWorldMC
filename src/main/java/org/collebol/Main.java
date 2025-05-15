8a3729cc9e5204035ae751380bcb0ae4de5fb16b
Colin Okhuijzen Mulder <contact@collebol.org>
2025-05-15 14:57:57 +0200

    Main class

--- src/main/java/org/collebol/Main.java
+++ src/main/java/org/collebol/Main.java
@@ -2,15 +2,32 @@ package org.collebol;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ *
+ * Main Kingdom World class.
+ *
+ * @author ColleBol - <a href="mailto:contact@collebol.org">contact@collebol.org</a>
+ * @since 1.0-dev
+ */
 public class Main extends JavaPlugin {
+
+    private Main plugin;
 
     @Override
     public void onEnable() {
         super.onEnable();
+
+        System.out.println("Kingdom World Plugin starting!");
+
+        this.plugin = this;
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    public Main instance() {
+        return this.plugin;
     }
 }
